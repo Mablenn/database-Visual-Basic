3516a66b25d1dc71f3f54ee2888735c136badea2
Mablenn <marcos.broseta@gmail.com>
2024-03-21 18:59:26 +0100

    Update Test02.vb

--- Test02.vb
+++ Test02.vb
@@ -1,1 +1,14 @@
+Imports System.Data.SqlClient
 
+' Ejemplo para establecer una sencilla conexión con una base de datos SQL Server
+' a través de la clase SqlConnection()
+Module Test02
+    Sub Main()
+        Dim cadenaConexion As String = "Data Source=MAB-PORTATIL\SQLEXPRESS;Initial Catalog=TestDB;Integrated Security=True"
+        Using myConnection As New SqlConnection(cadenaConexion)
+            ' Abre la conexión
+            myConnection.Open()
+            Console.WriteLine("Conexión establecida con la base de datos.")
+        End Using
+    End Sub
+End Module
