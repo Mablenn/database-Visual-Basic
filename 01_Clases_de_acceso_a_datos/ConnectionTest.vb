35332b77965ea49a16f0f2d63f0ebc04e24ee73f
Mablenn <marcos.broseta@gmail.com>
2024-03-22 10:59:26 +0100

    Update ConnectionTest.vb

--- 01_Clases_de_acceso_a_datos/ConnectionTest.vb
+++ 01_Clases_de_acceso_a_datos/ConnectionTest.vb
@@ -1,1 +1,29 @@
 
+Imports System.Data.SqlClient
+
+' Establece una conexión con una base de datos. Dependiendo del proveedor de la base de datos
+' esta clase puede tener un nombre distinto. Para SQL Server la clase se denomina SqlConnection()
+
+Module ConnectionTest
+  Sub Main()
+    Try
+      ' El objeto SqlConnection representa una conexión con un origen de datos 
+      Dim connection As SqlConnection = New SqlConnection()
+      
+      ' Al método ConnectionString() le asignamos la cadena de conexión a la base de datos         
+      connection.ConnectionString = "Data Source=MAB-PORTATIL\SQLEXPRESS;Initial Catalog=TestDB;Integrated Security=True"
+      
+      ' Un modo más eficiente sería asignar la cadena de conexión en el momento de crear la instancia
+      ' Dim connection As SqlConnection = New SqlConnection("Data Source=MAB-PORTATIL\SQLEXPRESS;Initial Catalog=TestDB;Integrated Security=True")
+      
+      ' Abre la conexión
+      connection.Open()
+      Console.WriteLine("Conexión establecida con la base de datos.")
+
+      ' Cierre de la conexión
+      connection.Close()
+    Catch ex As Exception
+      Console.WriteLine("Error: " & ex.Message)
+    End Try
+  End Sub
+End Module
