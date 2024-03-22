63713cfd7f22d9b812751c8d574615b83348f4cc
Mablenn <marcos.broseta@gmail.com>
2024-03-22 12:05:26 +0100

    Update DataReaderTest.vb

--- 01_Clases_de_acceso_a_datos/DataReaderTest.vb
+++ 01_Clases_de_acceso_a_datos/DataReaderTest.vb
@@ -1,1 +1,6 @@
+' La clase DataReader permite la lectura de un conjunto de resultados de una consulta SQL.
+' Hay que tener en cuenta que esta clase sólo permite lectura de datos y no modificación.
+' La clase DataReader que ofrece SQL Server es SqlDataReader.
+Imports System.Data.SqlClient
+
 
